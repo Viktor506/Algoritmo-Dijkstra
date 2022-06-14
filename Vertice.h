c2432bcf9913fc8a04107d999b513e216fcb45a7
Viktor506 <106695348+Viktor506@users.noreply.github.com>
2022-06-14 11:16:58 -0500

    Create Vertice.h

---
+++ Vertice.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+class Arista;
+
+class Vertice
+{
+	std::string nombre;
+	Vertice* sig;
+	Arista* ari;
+
+	friend class Grafo;
+public:
+	Vertice(std::string nombre);
+}; 
